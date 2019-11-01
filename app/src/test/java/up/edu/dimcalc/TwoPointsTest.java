7ca54110e2e0dc0ebf9030f800b36bb122f92754
sancheky22 <sancheky22@up.edu>
2019-10-31 17:35:52 -0700

    Did one test method that worked setOrigin

--- app/src/test/java/up/edu/dimcalc/TwoPointsTest.java
+++ app/src/test/java/up/edu/dimcalc/TwoPointsTest.java
@@ -4,6 +4,7 @@ import android.graphics.Point;
 
 import org.junit.Test;
 
+import static junit.framework.Assert.*;
 import static org.junit.Assert.*;
 
 public class TwoPointsTest {
@@ -37,18 +38,32 @@ public class TwoPointsTest {
     public void randomValue() {
         TwoPoints testPoints = new TwoPoints();
         testPoints.randomValue(0);
-        assertTrue(testPoints.getPoint(0)>=-10||testPoints.getPoint(0)<=10);
+        Point p1 = testPoints.getPoint(0);
+        int i = p1.x;
+//        assertTrue((p1.x >= 10));
+//        assertTrue(i >= -10);
+//        assertTrue(i <= 10);
     }
 
     @Test
     public void setOrigin() {
         TwoPoints testPoints = new TwoPoints();
-
+        testPoints.setOrigin(0);
+        Point p1 = testPoints.getPoint(0);
+        assertEquals(p1.x,0);
+        assertEquals(p1.y,0);
     }
 
     @Test
     public void copy() {
         TwoPoints testPoints = new TwoPoints();
+        testPoints.setPoint(0, 5, -3);
+        testPoints.setPoint(1, -3, 5);
+        Point p1 = testPoints.getPoint(0);
+        Point p2 = testPoints.getPoint(1);
+        testPoints.copy(0,1);
+        assertEquals(p2.x,p1.x);
+        assertEquals(p2.y,p1.y);
     }
 
     @Test
